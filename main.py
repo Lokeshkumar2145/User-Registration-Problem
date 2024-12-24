1e6bcf5509dfc73819448c7140d168e3279a76d9
Lokesh <lokeshkumark.2145@gmail.com>
2024-12-24 13:10:01 +0530

    Last name with Regx

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import re
 f_name=input("Enter the First name --> ")
-pattern=r"^[a-z][a-zA-z]{2,}$"
-if re.match(pattern,f_name):
+l_name=input("Enter the Last name --> ")
+pattern=r"^[A-Z][a-zA-z]{2,}$"
+if re.match(pattern,f_name) and re.match(pattern,f_name):
     print("Its a Valid Name")
 else:
     print("Its a In-valid Name")
+
+

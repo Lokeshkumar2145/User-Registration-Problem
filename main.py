365aaa10aa6992f1eed788022ca59ec438c9f1b3
Lokesh <lokeshkumark.2145@gmail.com>
2024-12-27 10:17:12 +0530

    email validation done

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import re
-f_name=input("Enter the First name --> ")
-l_name=input("Enter the Last name --> ")
+
 pattern=r"^[A-Z][a-zA-z]{2,}$"
-if re.match(pattern,f_name) and re.match(pattern,f_name):
-    print("Its a Valid Name")
-else:
-    print("Its a In-valid Name")
+emailpattern=r"^[\w_.]@[a-zA-z].[a-z]{3}.[a-z]{2}$"
 
+class Invalid(Exception):
+    pass
 
+def first_name(f_name,l_name):
+    if not (re.match(pattern,f_name) and re.match(pattern,l_name)):
+        raise Invalid ("Firstname and Lastname should start with cap and should be more than 3 letters")
+    return f"{f_name} {l_name} is a valid Name"
+def email(email):
+    if not (re.findall(pattern,email)):
+        raise Invalid ("Email should be in this format E.g. abc.xyz@bl.com")
+    return f"{email} is a valid Name"
+
+try:
+    x=input("Enter the First name --> ")
+    y=input("Enter the Last name --> ")
+    z=input("Enter the Email--> ")
+    print(first_name(x,y))
+    print(email(z))
+except Invalid as e:
+    print(e)
